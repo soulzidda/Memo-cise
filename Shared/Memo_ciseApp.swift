34a651e453f4f22e35fafe8f7092aad387f42e9c
Anthony Bassey <a.bassey@live.co.uk>
2021-07-28 17:48:10 +0200

    Initial Commit

---
+++ Shared/Memo_ciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memo_ciseApp.swift
+//  Shared
+//
+//  Created by Anthony Bassey on 2021-07-28.
+//
+
+import SwiftUI
+
+@main
+struct Memo_ciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
